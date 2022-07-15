a4c89ae26ce8d73f7495d3f1c99d3410f40d3e64
fajarfadillahh <fajaragustian979@gmail.com>
2022-07-15 20:49:28 +0700

    sticky header

--- src/js/main.js
+++ src/js/main.js
@@ -25,6 +25,16 @@ function scrollUp() {
 }
 window.addEventListener("scroll", scrollUp);
 
+// ===== STICKY HEADER =====
+function stickyHeader() {
+  const header = document.getElementById("header");
+
+  this.scrollY > 5
+    ? header.classList.add("sticky-action")
+    : header.classList.remove("sticky-action");
+}
+window.addEventListener("scroll", stickyHeader);
+
 // ===== TESTIMONIAL SWIPER =====
 let swiper = new Swiper(".testimonial-swiper", {
   centeredSlides: true,
